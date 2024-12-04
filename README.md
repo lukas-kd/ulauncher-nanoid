7ddc7bc7b413865cc799613ba13776e4d4886ef0
Lukas Kürzdörfer <luk@vfm-gruppe.de>
2024-12-04 14:31:41 +0100

    first version

---
+++ README.md
@@ -0,0 +1,46 @@
+# Ulauncher UUID
+
+> [Ulauncher](https://ulauncher.io/) Extension for generating UUID
+
+## Screenshots
+![media1](preview.gif)
+
+## Requirements
+
+* [ulauncher](https://ulauncher.io/)
+* Python >= 2.7
+* uuid (`pip install uuid`) and (`pip install uuid7`)
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```https://github.com/fsevenm/ulauncher-uuid```
+ 
+
+## Development
+
+```shell script
+git clone https://github.com/fsevenm/ulauncher-uuid
+ln -s <clone_location> ~/.local/share/ulauncher/extensions/ulauncher-uuid
+```
+
+To start debugging the extension, run these debugging script:
+```shell script
+# run in different Terminal tab
+ulauncher --no-extensions --dev -v
+# run in different Terminal tab, you need to change PYTHONPATH value based on your local python config, change the USERNAME to yours
+VERBOSE=1 ULAUNCHER_WS_API=ws://127.0.0.1:5054/ulauncher-uuid PYTHONPATH=/usr/lib/python3/dist-packages /usr/bin/python3 /home/USERNAME/.local/share/ulauncher/extensions/ulauncher-uuid/main.py
+```
+
+When you made any changes to `main.py` or the other assets, you need to re-run debugging scripts above.
+
+## Todos
+- [x] Add gif preview (or screenshot)
+- [ ] Add more namespaces for uuid v3 and v5
+- [x] Optimize gif preview to smaller size
+- [ ] Add different icon for each uuid version 
+
+## License 
+
+MIT @ Ayub Aswad
---
+++ main.py
@@ -0,0 +1,71 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from nanoid import generate
+import string
+
+class NanoIDExtension(Extension):
+    def __init__(self):
+        super(NanoIDExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, NanoIDQueryEventListener())
+
+class NanoIDQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        args = event.get_argument() or ""
+        args = args.split()
+
+        # Parse arguments
+        alphabet_type = args[0].lower() if len(args) > 0 else "readable"
+        size = args[1] if len(args) > 1 else "10"
+
+        # Set default alphabet
+        if alphabet_type == "readable":
+            alphabet = string.ascii_letters + string.digits
+        elif alphabet_type == "custom":
+            # Prompt user to provide custom alphabet
+            return RenderResultListAction([
+                ResultItem(
+                    title="Type your custom alphabet",
+                    subtitle="e.g., abcdef123",
+                    action=None  # No immediate action; wait for user input
+                )
+            ])
+        else:
+            # Invalid alphabet type
+            return RenderResultListAction([
+                ResultItem(
+                    title="Invalid alphabet type",
+                    subtitle="Choose either 'readable' or 'custom'",
+                    action=None
+                )
+            ])
+
+        # Validate size
+        try:
+            size = int(size)
+            if size < 2 or size > 12:
+                raise ValueError
+        except ValueError:
+            return RenderResultListAction([
+                ResultItem(
+                    title="Invalid size",
+                    subtitle="Enter a number between 2 and 12, or type a larger custom size",
+                    action=None
+                )
+            ])
+
+        # Generate NanoID
+        nanoid = generate(alphabet, size)
+        return RenderResultListAction([
+            ResultItem(
+                title=f"NanoID: {nanoid}",
+                subtitle="Click to copy to clipboard",
+                action=CopyToClipboardAction(nanoid)
+            )
+        ])
+
+if __name__ == '__main__':
+    NanoIDExtension().run()
